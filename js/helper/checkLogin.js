78994fab8edb38fb692bdd402501838534330327
SamGu <orz311013@gmail.com>
2023-12-17 13:21:01 +0800

    checkLogin

--- js/helper/checkLogin.js
+++ js/helper/checkLogin.js
@@ -1,31 +1,40 @@
 import { getItem } from "./StorageHandler";
 import processAxios from "./processAxios";
 
-export default async ()=>{
+export default async () => {
 
     const onLogin = document.querySelectorAll('.user--onLogin');
     const offLogin = document.querySelector('.user--offLogin');
-    
+
     //檢查token
     const accessToken = getItem('token');
     const expire = getItem('token-expire');
     //token有過就登入 反之 顯示登入註冊
-    if(accessToken && +expire > Date.now()){
-        
+    if (accessToken && +expire > Date.now()) {
+
         offLogin.style.display = 'none';
 
-        const result = await processAxios('get',`600/users/${getItem('userId')}`,'',true);
-        const { name , selfieImg } = result.data;
+        const result = await processAxios('get', `600/users/${getItem('userId')}`, '', true);
+        const { name, selfieImg } = result.data;
 
         const userPic = document.querySelectorAll('.userPic');
         const userName = document.querySelectorAll('.userPic + *');
-        userPic.forEach(x=>x.src = selfieImg ? `data:image/png;base64,${selfieImg.img64}` : '../assets/images/Unknown_person.jpg');
-        userName.forEach(x=>x.textContent = name);
+        userPic.forEach(x => x.src = selfieImg ? `data:image/png;base64,${selfieImg.img64}` : '../assets/images/Unknown_person.jpg');
+        userName.forEach(x => x.textContent = name);
     }
-    else{
-        onLogin.forEach(elem=>elem.classList.add('d-none'));
+    else {
+        onLogin.forEach(elem => elem.classList.add('d-none'));
     }
 };
 
 
 //第一次登入要引導至個人頁面編輯資料(待續)
+
+const logout = document.querySelector('button#logout');
+
+
+logout.addEventListener('click', function (e) {
+    e.preventDefault();
+    removeItem('token');
+    location.href = './index.html';
+})
